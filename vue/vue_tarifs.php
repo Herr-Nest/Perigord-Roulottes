665b4ea0655e7c13431ac4ee713d3f0769c03861
Dronis <florian.nicol@hotmail.fr>
2014-10-14 18:45:44 +0200

    Modification du CSS et UTF 8
    SET NAMES UTF8 pour la connexion.
    
    Et tableau centré d'Adrien
    
    Au passage, Adrien tu l'as ton calendrier maintenant.

--- vue/vue_tarifs.php
+++ vue/vue_tarifs.php
@@ -2,29 +2,31 @@
 	include_once('vue/header.php');
 ?>
 <section><br/>
-	<h1>Les Tarifs</h1>	
-	<table border=1>
-		<tr><th></th><th>Été</th><th>Printemps</th><th>Automne</th></tr>
-		<?php
-		$i=0;
-			foreach($lesTarifs as $unTarif){
-				if($i%3==0)
-				{
-					$sejour=$unTarif['NomSejour'];
-					echo "<tr><td>$sejour</td>";
-				}
+	<h1>Les Tarifs</h1>
+        <div id="tarif">
+            <table border=1>
+                    <tr><th></th><th>Été</th><th>Printemps</th><th>Automne</th></tr>
+                    <?php
+                    $i=0;
+                            foreach($lesTarifs as $unTarif){
+                                    if($i%3==0)
+                                    {
+                                            $sejour=$unTarif['NomSejour'];
+                                            echo "<tr><td>$sejour</td>";
+                                    }
 
-				$tarif=$unTarif['TarifSejour'];
-				echo "<td>$tarif</td>";
-				
-				if($i%3==2)
-				{
-					echo "</tr>";
+                                    $tarif=$unTarif['TarifSejour'];
+                                    echo "<td>$tarif</td>";
 
-				}
-				$i++;
+                                    if($i%3==2)
+                                    {
+                                            echo "</tr>";
 
-			}
-		?>
-	</table>
+                                    }
+                                    $i++;
+
+                            }
+                    ?>
+            </table>
+        </div><br />
 </section>
