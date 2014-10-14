a0b74e1e24bd23a54ca623d7bc8369ca1f344855
Herr-Nest <fonteneau.adrien@gmail.com>
2014-10-14 14:49:31 +0200

    ajout connexion bdd dans l'index + redirection vers controleur page d'accueil

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <?php
 
-    include('vue/header.php') ;
+	include_once('modele/connexionBdd.php');
+	
+	if (!isset($_GET['section']) OR $_GET['section'] == 'index')
+	{
+
+    	include_once('controleur/Accueil.php');
+
+	}
+
+?>
 
