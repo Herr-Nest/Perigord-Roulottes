d5e56a8c07b69b1da839202b1cd2cd17c675c582
Dronis <florian.nicol@hotmail.fr>
2014-10-14 14:32:19 +0200

    Design_basique

---
+++ vue/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+            <meta charset="utf-8">
+            <link rel="stylesheet" href="vue/style.css" />
+            <title>Accueil</title>
+    </head>
+	
+    <body>
+        <header>
+            <img src="vue/img/banniere_test.png" alt="banniere" />
+        </header>
+        <nav>
+            <ul>
+                <li>Accueil</li><!--
+                --><li>Item_2</li><!--
+                --><li>Item_3</li><!--
+                --><li>Item_4</li><!--
+                --><li>Item_5</li><!--
+            --></ul>
+        </nav>
+    </body>
