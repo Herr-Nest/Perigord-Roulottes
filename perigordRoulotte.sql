f5b1ce5143cd5a9cc5cfe52e64d49065eb75dc7a
Herr-Nest <fonteneau.adrien@gmail.com>
2014-10-17 11:09:26 +0200

    maj bdd

--- perigordRoulotte.sql
+++ perigordRoulotte.sql
@@ -84,6 +84,7 @@ CREATE TABLE IF NOT EXISTS `etape` (
   `VilleEtape` varchar(25) COLLATE utf8_unicode_ci DEFAULT NULL,
   `NomHeb` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
   `SiteInternet` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `DescriptionEtape` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `EtapeSuivante` int(11) DEFAULT NULL,
   PRIMARY KEY (`NumEtape`),
   KEY `FK_Etape_NumEtape_1` (`EtapeSuivante`)
@@ -93,14 +94,14 @@ CREATE TABLE IF NOT EXISTS `etape` (
 -- Contenu de la table `etape`
 --
 
-INSERT INTO `etape` (`NumEtape`, `VilleEtape`, `NomHeb`, `SiteInternet`, `EtapeSuivante`) VALUES
-(1, 'Brantome', 'Camping de Brantome', 'www.CampingdeBrantome.fr', NULL),
-(2, 'St Pardoux', 'Camping de St Pardoux', 'www.CampingdeStPardoux.fr', NULL),
-(3, 'St Saud Lacousiere', 'Camping de St Saud Lacousiere', 'www.CampingdeStSaudLacousiere.fr', NULL),
-(4, 'Bussiere Badil', 'Camping de Bussiere Badil', 'www.CampingdeBussiereBadil.fr', NULL),
-(5, 'Beaussac', 'Camping de Beaussac', 'www.CampingdeBeaussac.fr', NULL),
-(6, 'Monsec', 'Camping de Monsec', 'www.CampingdeMonsec.fr', NULL),
-(7, 'Bourdeilles', 'Camping de Bourdeilles', 'www.CampingdeBourdeilles.fr', NULL);
+INSERT INTO `etape` (`NumEtape`, `VilleEtape`, `NomHeb`, `SiteInternet`, `DescriptionEtape`, `EtapeSuivante`) VALUES
+(1, 'Brantome', 'Camping de Brantome', 'www.CampingdeBrantome.fr', 'Pour un agréable séjour en famille, le camping de Brantome vous accueille au coeur du Perigord.Cette petite structure tout confort de 40 emplacements.', 2),
+(2, 'St Pardoux', 'Camping de St Pardoux', 'www.CampingdeStPardoux.fr', NULL, 3),
+(3, 'St Saud Lacousiere', 'Camping de St Saud Lacousiere', 'www.CampingdeStSaudLacousiere.fr', NULL, 4),
+(4, 'Bussiere Badil', 'Camping de Bussiere Badil', 'www.CampingdeBussiereBadil.fr', NULL, 5),
+(5, 'Beaussac', 'Camping de Beaussac', 'www.CampingdeBeaussac.fr', NULL, 6),
+(6, 'Monsec', 'Camping de Monsec', 'www.CampingdeMonsec.fr', NULL, 7),
+(7, 'Bourdeilles', 'Camping de Bourdeilles', 'www.CampingdeBourdeilles.fr', NULL, 1);
 
 -- --------------------------------------------------------
 
