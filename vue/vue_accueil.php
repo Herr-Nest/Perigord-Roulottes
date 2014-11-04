be4a0fd852d651d4e900df0a80819e5631655846
Herr-Nest <fonteneau.adrien@gmail.com>
2014-11-04 14:20:42 +0100

    correct lien img

--- vue/vue_accueil.php
+++ vue/vue_accueil.php
@@ -10,33 +10,8 @@
 		<li><a href="vue/img/paysagePerigord.jpg" data-lightbox="perigordRoulottes"><img src="vue/img/paysagePerigord.jpg" class="thumbnail"/></a></li>
 		<li><a href="vue/img/paysagePerigord2.jpg" data-lightbox="perigordRoulottes"><img src="vue/img/paysagePerigord2.jpg" class="thumbnail"/></a></li>
 		<li><a href="vue/img/roulotte.jpg" data-lightbox="perigordRoulottes"><img src="vue/img/roulotte.jpg" class="thumbnail"/></a></li>
-		<li><a href="vue/img/roulottes2.jpg" data-lightbox="perigordRoulottes"><img src="vue/img/roulotte2.jpg" class="thumbnail"/></a></li>
+		<li><a href="vue/img/roulotte2.jpg" data-lightbox="perigordRoulottes"><img src="vue/img/roulotte2.jpg" class="thumbnail"/></a></li>
 	</ul>
 	</div>
-		<br/><h1>Bienvenue dans le site Perigord Roulottes </h1><br/>
-		<p>Bienvenue dans le site Perigord Roulottes<br />
-		Bienvenue dans le site Perigord Roulottes<br />
-		Bienvenue dans le site Perigord Roulottes<br />
-		Bienvenue dans le site Perigord Roulottes<br />
-		Bienvenue dans le site Perigord Roulottes<br />
-		Bienvenue dans le site Perigord Roulottes<br />
-		Bienvenue dans le site Perigord Roulottes<br />
-		Bienvenue dans le site Perigord Roulottes<br />
-		Bienvenue dans le site Perigord Roulottes<br />
-		Bienvenue dans le site Perigord Roulottes<br />
-		Bienvenue dans le site Perigord Roulottes<br />
-		Bienvenue dans le site Perigord Roulottes<br />
-		Bienvenue dans le site Perigord Roulottes<br />
-		Bienvenue dans le site Perigord Roulottes<br />
-		Bienvenue dans le site Perigord Roulottes<br />
-		Bienvenue dans le site Perigord Roulottes<br />
-		Bienvenue dans le site Perigord Roulottes<br />
-		Bienvenue dans le site Perigord Roulottes<br />
-		Bienvenue dans le site Perigord Roulottes<br />
-		Bienvenue dans le site Perigord Roulottes<br />
-		Bienvenue dans le site Perigord Roulottes<br />
-		Bienvenue dans le site Perigord Roulottes<br />
-		Bienvenue dans le site Perigord Roulottes<br />
-		Bienvenue dans le site Perigord Roulottes<br />
-		</p><!--
+		<br/><h1>Bienvenue dans le site Perigord Roulottes </h1><br/><!--
 	--></section>
