98f1ca3f57e9e99fef5a5393874b64fd37ac31bc
MrtPi <99MW66@Gmail.com>
2014-11-18 15:30:25 +0100

    Recap Reservation

---
+++ vue/vue_recap_reservation.php
@@ -0,0 +1,12 @@
+<?php
+	
+	include_once('header.php') ;
+?>
+<section>
+    <h1>Réservation</h1>
+
+		<?php
+			
+			echo"$nom <br/> $prenom <br/> $Adresse <br/> $CP <br/> $VilleCli <br/> $Date <br/> $VilleDep <br/> $Duree <br/>";
+			
+		?> 
