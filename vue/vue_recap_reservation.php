d2782a1ff4ae912553fccfa670326220c30c6cbe
Herr-Nest <fonteneau.adrien@gmail.com>
2014-11-21 12:00:52 +0100

    controle form reserv js

--- vue/vue_recap_reservation.php
+++ vue/vue_recap_reservation.php
@@ -3,12 +3,15 @@
 	include_once('header.php') ;
 ?>
 <section id="reservation">
-    <h1>Réservation Client</h1>
+    <h1>Récapitulatif demande Réservation</h1>
 
 		<?php
 			
 			echo"<div id='floatleft'><img src='vue/img/logo.jpg'  /></div>";
-			echo"   <div id='details'>Nom  : $nom <br/> Prénom  : $prenom <br/> Adresse  : $adresse <br/> Code Postal  : $cp <br/> Ville  : $ville <br/> Date Départ  : $datedepart <br/> Village Départ  :$villedepart <br/> Nombres de Jours  : $nbJours <br/></div>";
+			echo"   <div id='details'>Nom  : $nom <br/> Prénom  : $prenom <br/> Adresse  : $adresse <br/>"
+                                . " Code Postal  : $cp <br/> Ville  : $ville <br/> Date Départ  : $datedepart <br/> "
+                                . "Village Départ  :$villedepart <br/> Nombres de Jours  : $nbJours <br/><br/>"
+                                . "<input type=button value='envoyer la demande' onclick=\"alert('fonctionnalité encore en développement')\"</div>";
 			
 		?>
 		
--- vue/vue_reservation.php
+++ vue/vue_reservation.php
@@ -1,6 +1,33 @@
 <?php
     include_once('header.php') ;
 ?>
+<script>
+	function verifSaisie()
+	{
+		
+		var ok=true;
+		if((formReservation.nomclient.value=="") || (formReservation.prenomclient.value=="") || (formReservation.adresseclient.value=="") || (formReservation.cpclient.value=="") || (formReservation.villeclient.value=="") || (formReservation.date.value==""))
+		{
+			alert('veuillez remplir tous les champs');
+			ok=false;
+		}
+		if((formReservation.cpclient.value.length!=5) || (isNaN(formReservation.cpclient.value)))
+		{
+			alert('le code postal est incorrect');
+			ok=false;
+		}
+		if(ok)
+		{
+			formReservation.submit();
+		}else{
+			return ok;
+		}
+		
+
+	}
+
+</script>
+
 <section>
     <h1>Pré-réservation</h1>
     <br />
@@ -17,16 +44,16 @@
     </p>
     <br />
     <div id="form_reserv">
-        <form method="post" action="index.php?section=reserv_roul">
+        <form name='formReservation'method="post" action="index.php?section=reserv_roul">
             <h3>Vos coordonnées</h3>
-            <p>Nom : <input type="text" name="nomclient" class="texte" required/></p>
-            <p>Prenom : <input type="text" name="prenomclient" class="texte" required/></p>
-            <p>Adresse : <input type="text" name="adresseclient" class="texte" required/></p>
-            <p>Code Postal : <input type="text" name="cpclient" class="texte" required/></p>
-            <p>Ville : <input type="text" name="villeclient" class="texte" required/></p>
+            <p>Nom : <input type="text" name="nomclient" class="texte"/></p>
+            <p>Prenom : <input type="text" name="prenomclient" class="texte" /></p>
+            <p>Adresse : <input type="text" name="adresseclient" class="texte" /></p>
+            <p>Code Postal : <input type="text" name="cpclient" class="texte" /></p>
+            <p>Ville : <input type="text" name="villeclient" class="texte" /></p>
             <hr />
             <h3>Détails du séjour</h3>
-            <p>Date de départ : <input type="text" name="date" id="date" class="calendrier" value="Cliquez ici pour afficher le calendrier" required/></p>
+            <p>Date de départ : <input type="text" name="date" id="date" class="calendrier" placeholder="Cliquez ici pour afficher le calendrier" readonly /></p>
             <p>Durée du séjour : <select name="dureesej">
                 <?php
                     foreach($liste_sejours as $unsejour)
@@ -44,8 +71,7 @@
                 ?>
                 
                 </select></p><br/>
-            <p><input type="submit" value="Valider" class="button"/></p><br />
+            <p><input type="button" value="Valider" class="button" onclick="verifSaisie()"/></p><br />
         </form>
     </div>
 </section>
-
